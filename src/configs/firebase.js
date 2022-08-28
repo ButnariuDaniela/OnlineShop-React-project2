a3bece8a45636eae86bb06020718b420fddd43a6
Dana2025 <horlescu_dana@yahoo.com>
2022-08-28 14:56:21 +0300

    Modify firebaseConfig

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCcbZMkwd4ZV_1DiH0R3TZFCg9GKaIMeoM",
-    authDomain: "react-sirluggia-shop.firebaseapp.com",
-    databaseURL: "https://react-sirluggia-shop.firebaseio.com",
-    projectId: "react-sirluggia-shop",
-    storageBucket: "react-sirluggia-shop.appspot.com",
-    messagingSenderId: "184253035461",
-    appId: "1:184253035461:web:20fe5c173c7675c74c51ac"
-};
+    apiKey: "AIzaSyAkpxQlpWNYWndj-Gn-Ym3Diy8KyZsNzd8",
+    authDomain: "fronturi-mdf.firebaseapp.com",
+    projectId: "fronturi-mdf",
+    storageBucket: "fronturi-mdf.appspot.com",
+    messagingSenderId: "780602704796",
+    appId: "1:780602704796:web:77dff166e659de3e527e59"
+    };
 
 export default firebaseConfig;
