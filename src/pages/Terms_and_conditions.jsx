111b85ff50b5521a3303de8accbd72263bb7628b
Dana2025 <horlescu_dana@yahoo.com>
2022-08-31 00:36:24 +0300

    Firt modify on json

--- src/pages/Terms_and_conditions.jsx
+++ src/pages/Terms_and_conditions.jsx
@@ -3,9 +3,38 @@ import Layout from '../components/Layout';
 
 function Terms_and_conditions() {
     return (
-        <div>
+        <div className='ml-5 mr-5'>
             <Layout>
-                <h1>Termeni si conditii</h1>
+                <h2>ASPECTE GENERALE</h2>
+                <p>Acest site apartine societatii SC Open Space SRL, cu sediul social in Str. Ion Nanu nr.31, Roman, Jud. Neamt, inregistrata sub nr. J27/1381/1993  , CUI RO4324999</p>
+                <h3>Termeni si conditii</h3>
+                <p>Scopul principal este de a oferi informatii cu privire la serviiciile si produsele realizate de societatea noastra. Aceste informatii au caracter informativ.</p>
+                <p>Accesul la prezentul site precum si folosirea acestuia sunt permise numai in concordanta cu termenii si conditiile de mai jos, precum si cu prevederile legale aplicabile in acest domeniu.</p>
+                <p>In calitate de proprietar al site-ului,  SC Open Space SRL isi rezerva dreptul de a actualiza sau modifica termenii si condiitiile  fara o notificare prealabila .</p>
+                <p>In cazul in care nu sunteti de acord cu conditiile de utilizare a site-ului, care implica si prelucrarea datelor cu caracter personal de catre SC Open Space SRL, va rugam sa nu utilizati acest site.  </p>
+                <p>Toate solicitarile legate de datele dumneavoastra, de protectia datelor in cadrul societatii SC Open Space SRL pot fi adresate catre adresa de email: horlescu_dana@yahoo.</p>
+                <p>Acceptarea termenilor si conditiilor este realizata, in momentul abonarii la newsletter, odata cu selectarea optiunii "Am luat cunostinta si accept Termenii si Conditiile". Selectarea acesteia si a butonului Trimitere sub forma unei imagini tip plic, indica faptul ca sunteti de acord cu respectarea termenilor si conditiilor SC Open Space SRL pe care le-ati citit, le-ati inteles si le-ati acceptat in integralitate. De asemenea, acceptarea termenilor si conditiilor se realizata si in formularul de contact, odata cu selectarea selectarea optiunii "Am luat cunostinta si accept Termenii si Conditiile". Selectarea acesteia si a butonului Trimite, indica faptul ca sunteti de acord cu respectarea termenilor si conditiilor SC Open Space SRL.</p>
+                <h3>Newsletter</h3>
+                <p>Prin trimiterea datelor dumneavoastra pe site si selectarea optiunii "Am luat cunostinta si accept Termenii si Conditiile", indica faptul ca sunteti de acord cu respectarea termenilor si conditiilor SC Open Space SRL pe care le-ati citit, le-ati inteles si le-ati acceptat in integralitate. Astfel SC Open Space SRL va poate trimite periodic prin intermediul newsletter-elor in format e-mail sau SMS, informatii referitoare la serviciile si produsele furnizate de catre acesta.</p>
+                <p>Acceptul dumneavoastra poate fi oricand retras prin simpla trimitere a unei notificari la SC Open Space SRL la adresa de email horlescu_dana@yahoo.com.</p>
+                <h3>Date cu caracter personal</h3>
+                <ul>SC Open Space SRL colecteaza prin intermediul prezentului site urmatoarele date cu caracter personal furnizate direct de catre dumneavoastra:
+                    <li>Adresa de email – in formularele de abonare la newsletter</li>
+                    <li>Nume, prenume, adresa de email, telefon in formularul de contact</li>
+                </ul>
+                <h3>Scopurile Prelucrarii</h3>
+                <ul>SC Open Space SRL prelucreaza datele dumneavoastra cu caracter personal in masura permisa sau impusa de legislatia aplicabila in urmatoarele scopuri:</ul>
+                <li>Pentru trimiterea de newsletter-e cuprinzand informatii referitoare la produsele si/sau serviciile oferite de catre SC Open Space SRL</li>
+                <li>Pentru a permite contactarea si administrarea contractarii potentialilor parteneri/clienti SC Open Space SRL</li>
+                <h3>Drepturile dumneavoastra</h3>
+                <ul>In cazul in care ati fost de acord cu prelucrarea anumitor date de catre SC Open Space SRL, puteti retrage acest consimtamant in orice moment avand numai efecte viitoare. Aceasta actiune se face prin trimiterea unei notificari scrise la adresa de email horlescu_dana@yahoo:</ul>
+                <li><b>Dreptul de acces: </b> Prin acest drept, puteti inainta o cerere scrisa pentru a vi se comunica daca Datele dumneavoastra cu caracter personal sunt prelucrate. In caz afirmativ puteti cere o copie a acestora.</li>
+                <li> <b>Dreptul la rectificare: </b>Prin acest drept puteti rectifica orice informatie incorecta din Datele dumneavoastra cu caracter personal, bineinteles prin intermediul unei declaratii scrise.</li>
+                <li><b>Dreptul la stergere („dreptul de a fi uitat”):  </b>Prin acest drept, puteti sa ne solicitati, in scris, prin trimiterea unui email la adresa horlescu_dana@yahoo</li>
+                <li><b>Dreptul la restrictionare: </b>Aveti dreptul de a solicita restrictionarea Prelucrarii Datelor dumneavoastra cu caracter personal. in acest caz, datele respective vor fi marcate si pot fi prelucrate de noi doar in anumite scopuri.</li>
+                <h3>Cookie-uri</h3>
+                <p>Prezenta sectiune referitoare la Cookies se aplica tuturor utilizatorilor acestui Site. Prin termenul „cookie”-uri intelegem modulele cookie si tehnologiile similare prin intermediul carora pot fi colectate informatii in mod automat.</p>
+                <p>Acest site nu foloseste „cookie”-uri.</p>
             </Layout>
         </div>
     )
